8cfcd5abae50f89ccb302b5ada95ae4bc6e88f01
Joseph22-sys <130204527+Joseph22-sys@users.noreply.github.com>
2025-03-19 20:04:20 +0100

    UpdateToDatabae main.py

--- flask-vowel-finder/main.py
+++ flask-vowel-finder/main.py
@@ -1,11 +1,36 @@
 from flask import Flask, render_template, request, redirect
 from vowelfinder import search4letters
+import mysql.connector 
+
+
+dbconfig = { 
+            'host': '127.0.0.1', 
+            'user': 'root', 
+            'password': 'personal_tin', 
+            'database': 'vsearchdb', 
+        }
+         
+
+conn = mysql.connector.connect(**dbconfig)
+cursor = conn.cursor()
+
 
 app = Flask(__name__)
 
-def log_request(req:'flask_request', res: str) -> None: 
-    with open('vsearch.log', 'a') as log: 
-        print(f"The IP Address {req.remote_addr} using the browser name {req.user_agent} inputted {req.form} and the Result was {res}", file=log, sep="|")
+def log_request(req: 'flask_request', res: str) -> None:
+    """Log details of the web request and the results."""
+    try:
+        browser_string = req.user_agent.browser or 'Unknown'
+        
+        _SQL = """insert into log (phrase, letters, ip, browser_string, results) 
+                  values (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'], req.form['letter'], 
+                              req.remote_addr,browser_string, res))
+        conn.commit()
+    except mysql.connector.Error as e:
+        print(f"Error logging request: {e}")
+    
+
 
 @app.route("/")
 def home():
@@ -17,18 +42,23 @@ def entry():
 
 @app.route("/result", methods=["POST"])
 def results():
+
     phrase = request.form["phrase"]
     letter = request.form["letter"]
-    result = search4letters(phrase, letter)
-    log_request(request, result) 
+    result = str(search4letters(phrase, letter))
+    log_request(request,result)
     return render_template("result.html", phrase=phrase, letter=letter, page_title="Result", result=result)
 
-@app.route("/logWriter")
-def log_writer():
-    with open("vsearch.log", "r") as log_file:
-        content = log_file.readline()
-    return content
+
+    
+@app.route("/viewlogs")
+def viwelog():
+    _SQL = """select * from log"""
+    cursor.execute(_SQL)
+    return cursor.fetchall()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
